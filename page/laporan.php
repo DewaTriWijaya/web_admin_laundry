4465db7749961379762851f81a70226a98cdb2c4
Gumilarmn <Gumilarmn16@gmail.com>
2024-07-21 14:51:52 +0700

    front end laporan

--- page/laporan.php
+++ page/laporan.php
@@ -7,14 +7,46 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/bootstrap.min.css" />
-
-    <title>Document</title>
+    <title>Laporan</title>
 </head>
-<body>
-    <h1>Halaman Laporan</h1>
-    <h2>Coba Push Branch Laporan</h2>
-    <h3>Aku rubah lagi ya</h3>
-    <script src="../js/bootstrap.min.js"></script>
+<body class="bg-secondary" style="--bs-bg-opacity: .15;">
+<div class="container">
+        <div class="row">
+            <h2 class="fw-bold mb-5">Laporan Laundry</h2>
+            <h4 class="mb-4">Masukkan Jangka Waktu</h4>
+            <d class="d-flex gap-4 mb-5">
+                <div class="d-flex gap-4 ">
+                    <input type="date" class="form-control">
+                    <input type="date" class="form-control">
+                </div>
+            <button type="button" class="btn btn-success px-4">Cari</button>
+            </div>
+
+            <h4>Transaksi</h4>
+            <div class="d-flex justify-content-center">
+                <table class="table table-borderless">
+                    <thead class="text-center">
+                        <tr>
+                            <th scope="col">No. Nota</th>
+                            <th scope="col">Total Harga</th>
+                            <th scope="col">Bulan</th>
+                            <th scope="col">Tahun</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-light">
+                        <tr>
+                            <td scope="row" class="text-center">0112</td>
+                            <td scope="row" class="text-center">Rp. 75,000</td>
+                            <td scope="row" class="text-center">Rp. 75,000</td>
+                            <td scope="row" class="text-center">Rp. 75,000</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="d-flex justify-content-start mt-4">
+                <button type="button" class="btn btn-success px-4">Unduh</button>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
