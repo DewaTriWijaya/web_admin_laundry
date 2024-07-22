eb24f09fc81a4294689e3dc35550688ea0d6a56e
Gumilarmn <Gumilarmn16@gmail.com>
2024-07-22 10:45:44 +0700

    add favicon, change first page

--- index.php
+++ index.php
@@ -13,7 +13,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <title>Document</title>
+    <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
+    <title>Laundry</title>
 </head>
 <body>
     <div class="d-flex vh-100">
@@ -99,7 +100,7 @@
                 include 'page/home.php';
             }
         } else {
-
+            include 'page/pendaftaran.php';
         }
             // Hapus { dan perbaiki penutup <section> dan komentar
         ?>
